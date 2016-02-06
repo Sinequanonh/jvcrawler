bf05278647c7bbc75e80008f1682ea3146277493
Léo Baecker <leo.baecker@epitech.eu>
2016-02-06 17:10:08 +0100

    Grouped requests Grouped inserts Get

--- tempsreel.py
+++ tempsreel.py
@@ -5,13 +5,13 @@ import re
 import thread
 import time
 import datetime
+import math
 import requests
 import gevent
 import grequests
 import HTMLParser
 import json
 from bs4 import BeautifulSoup, SoupStrainer
-#from BeautifulSoup import BeautifulSoup, SoupStrainer
 from pymongo import MongoClient
 import pymongo
 
@@ -21,8 +21,8 @@ def insertPseudo(pseudo):
     client = MongoClient()
     # Database
     db = client['jvcrawler']
-    # Insert post to database
-#    db.pseudo.insert_one(pseudo).inserted_id
+    #Insert post to database
+    #db.pseudo.insert_one(pseudo).inserted_id
     db.pseudo.insert_many(pseudo)
 
 def crawler():
@@ -45,6 +45,7 @@ def crawler():
 
         # Parse each <li>
         topics = []
+        link_list = []
         for topic in les_topics:
             #Get info from topic url
             topic_name = topic.find('a', 'lien-jv topic-title')
@@ -65,52 +66,90 @@ def crawler():
                             'id': topic_id,
                             'author': topic_author
                             })
-            print "======================="
-
-
+                    print topic_author
+                    topic_nbmsg = topic.find('span', 'topic-count')
+                    topic_nbmsg = topic_nbmsg.text.replace(' ', '').replace('\n', '')
+                    if topic_nbmsg:
+                        print topic_nbmsg
+                        topic_nbpage = float(topic_nbmsg) / 20
+                        topic_nbpage = int(math.ceil(topic_nbpage))
+                        if topic_nbpage == 0:
+                            topic_nbpage = 1
+                        print topic_nbpage
+                        # Assign last page of the topic
+                        topic_lastpage = topic_link.split('-')
+                        topic_lastpage[3] = str(topic_nbpage)
+                        topic_lastpage = '-'.join(topic_lastpage)
+                        print topic_lastpage
+                        link_list.append('https://www.jeuxvideo.com/' + topic_lastpage)
+                        
+            print "================================================"
         # Async requests the 25 topics all in once
-#        rs = (grequests.get(u) for u in link_list)
- #       responses = grequests.map(rs)
+        rs = (grequests.get(u) for u in link_list)
+        response = grequests.map(rs)
+        get_pages(response, topics, link_list)
 
 #        get_pseudos(response)
         i+=1
         return
 
 # from the first page of a topic
-def get_pages(response):
-    nextStrainer = SoupStrainer('div', 'pagi-after')
-    next = BeautifulSoup(response.text, parseOnlyThese=nextStrainer)
-    print next
-#    caca =[tag for tag in BeautifulSoup(response.text, parseOnlyThese=nextStrainer)]
-#   for prout in caca:
- #       print prout['href']
-    
+def get_pages(response, topics, link_list):
+    i = 0
+    nb_link = 0
+    nb_requests = 0
+    for res in response:
+        # get each page from the last
+        list_pages = []
+        while int(link_list[i].split('-')[3]) > 0:
+            previous = link_list[i].split('-')
+            previous_page = int(previous[3])
+            previous_page -= 1
+            previous[3] = str(previous_page)
+            link_list[i] = '-'.join(previous)
+            list_pages.append(link_list[i])
+            print link_list[i]
+            if nb_requests > 8:
+                rs = (grequests.get(u) for u in list_pages)
+                laresponse = grequests.map(rs)
+                get_pseudos(laresponse)
+                nb_requests = 0
+                del list_pages[:]
+            nb_requests+=1
+            
+        rs = (grequests.get(u) for u in list_pages)
+        laresponse = grequests.map(rs)
+        get_pseudos(laresponse)
+        res.close()
+        i+=1
     return
-
+        
+    
 def get_post():
     return
 
 def get_pseudos(response):
     i = 1
-    nb_pseudos = 0
+    nb_insert = 0
+    print "SIZE OF RES" + str(len(response))
+    pseudo = SoupStrainer('div',{'class': 'bloc-header'})
     for link in response:
         print str(i) + " ==================================================="
         i+=1
-        pseudo = SoupStrainer('div',{'class': 'bloc-header'})
-        try:
-            soup = BeautifulSoup(link.text, parseOnlyThese=pseudo)
-        except:
-            pass
+        soup = BeautifulSoup(link.text, parseOnlyThese=pseudo)
         pseudal = soup.find_all('span', attrs={'class':'bloc-pseudo-msg'})
         list_pseudos = []
         for pseud in pseudal:
             pseud = pseud.getText().replace(' ', '').replace('\n', '')
-            print pseud
             pseudotoinsert = {"pseudo": pseud}
-            list_pseudos.insert(nb_pseudos, pseudotoinsert)
-            nb_pseudos+=1
-#        print list_pseudos
-        insertPseudo(list_pseudos)
+            list_pseudos.insert(nb_insert, pseudotoinsert)
+            #insertPseudo(pseudotoinsert)
+            nb_insert+=1
+        print list_pseudos
+        #insertPseudo(list_pseudos)
+    link.close()
+
+        
 
 start_time = time.time()
 crawler()
