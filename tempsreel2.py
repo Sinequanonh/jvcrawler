f026b438cc2cec6be2871d13fc3ea28d6f704f34
Léo Baecker <leo.baecker@epitech.eu>
2016-02-13 04:15:51 +0100

    Parse and insert a post

--- tempsreel2.py
+++ tempsreel2.py
@@ -21,7 +21,7 @@ client = MongoClient()
 # Database
 db = client['jvcrawler']
 db.pseudo.ensure_index("pseudo", unique = True)
-#db.bears.create_index("pseudo")
+db.jvstalker.ensure_index("ancre", unique = True)
 
 ###
 # FUNCTIONS
@@ -104,14 +104,16 @@ def fromLastPage(bulk_request, link_list):
 			print link_list[i]
 			if nb_requests > 1:
 				r = bulkRequests(list_pages)
-				get_pseudos(r)
+				get_messages(r)
+				#get_pseudos(r)
 				del list_pages[:]
 				nb_requests = 0
 			nb_requests+=1
 		res.close()
 		i+=1
 	r = bulkRequests(list_pages)
-	get_pseudos(r)
+	#get_pseudos(r)
+	get_messages(r)
 
 	elapsedTime(start_time)
 	return
@@ -123,24 +125,31 @@ def parseMessages():
 	elapsedTime(start_time)
 	return
 
-def singleInsertDatabase(pseudo):
+def insertPseudo(pseudo):
 	start_time = time.time()
 	debugFunction()
+	pseudotoinsert = {"pseudo": pseudo['pseudo'],
+                	  "nb_msg": 1}
 	try:
-		db.pseudo.insert_one(pseudo).inserted_id
+		db.pseudo.insert_one(pseudotoinsert).inserted_id
 	except:
-		db.pseudo.update_one({
-					  'pseudo': pseudo['pseudo']
-					},{
+		pass
+		db.pseudo.update_one({'pseudo': pseudo['pseudo']}, {
 					  '$inc': {
 					    'nb_msg': 1
 					  }
 					}, upsert=False)
-		#db.pseudo.findAndModify({
-			#query: {"pseudo": pseudo},
-			#update: {$inc: {"nb_msg": 1}}
-			#})
+	elapsedTime(start_time)
+	return
+
+def insertPost(post):
+	start_time = time.time()
+	debugFunction()
+	try:
+		db.jvstalker.insert_one(post).inserted_id
+	except:
 		pass
+	insertPseudo(post)
 	elapsedTime(start_time)
 	return
 
@@ -157,17 +166,45 @@ def bulkInsertDatabase(pseudos):
 	elapsedTime(start_time)
 	return
 
+def get_messages(response):
+	bloc_message = SoupStrainer('div', {'class': 'inner-head-content'})
+	bloc_ancre = SoupStrainer('span', {'class': 'bloc-message-forum-anchor'})
+	for res in response:
+		soup = BeautifulSoup(res.text, "html.parser", parse_only=bloc_message)
+		soup_ancre = BeautifulSoup(res.text, "html.parser", parse_only=bloc_ancre)
+		ancres = soup_ancre.find_all('span')
+		i = 0
+		for s in soup:
+			# Pseudo
+			pseudo = s.find('span', attrs={'class': 'bloc-pseudo-msg'})
+			pseudo = pseudo.getText().replace(' ', '').replace('\n', '')
+			# Message
+			message = s.find('div', attrs={'class': 'text-enrichi-forum'}).renderContents()
+			# Date
+			date = s.find('div', attrs={'class': 'bloc-date-msg'})
+			date = date.text.replace(' ', '').replace('\n', '')
+			# Ancre
+			ancre = ancres[i]['id'].split('_')[1]
+			print ancre
+			pseudotoinsert = {"pseudo": pseudo, "message": message, "date": date, "ancre": ancre}
+			insertPost(pseudotoinsert)
+			i+=1
+
+	return
+
 def get_pseudos(response):
     i = 1
     nb_insert = 0
     print "SIZE OF RES" + str(len(response))
-    pseudo = SoupStrainer('div',{'class': 'bloc-header'})
+    pseudo = SoupStrainer('div', {'class': 'bloc-header'})
     for link in response:
         if link:
             print str(i) + " ==================================================="
             i+=1
-            soup = BeautifulSoup(link.text, parse_only=pseudo)
+            soup = BeautifulSoup(link.text, "html.parser", parse_only=pseudo)
             pseudal = soup.find_all('span', attrs={'class':'bloc-pseudo-msg'})
+            blocmessage = soup.find_all('div', attrs={'class':'text-enrichi-forum'})
+            message = blocmessage[i].renderContents()
             list_pseudos = []
             nb_insert = 0
             for pseud in pseudal:
